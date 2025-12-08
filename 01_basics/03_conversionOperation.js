014fa0789be8cb5fea9a183181d6a6718c560e3d
Anil Kumar Mullangi <160340131+Anilkumarmullangi@users.noreply.github.com>
2025-12-08 06:25:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score="anil"
+
+//console.log(typeof score)
+
+let valueInNumber =Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+/*
+ "33" => 33
+ "33abe" => NaN
+ true =>1; false=0
+ */
+
+let isLoggedIn=1;
+
+let BooleanIsloggedIn=Boolean (isLoggedIn)
+//console.log(BooleanIsloggedIn);
+
+/*
+1 => true ; false=> 0;
+"anil" =>true;
+"" =>false;
+*/
+
+let someNumber=33
+
+let StringNumber=String(someNumber)
+console.log(typeof StringNumber);
+
