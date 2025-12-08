6c97aa3130f2de904196025b9d435674c512a8ad
Anil Kumar Mullangi <160340131+Anilkumarmullangi@users.noreply.github.com>
2025-12-08 05:57:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 +3) // we are using node.js not browser
+
+console.log(3
+    +
+    3)//code readability should be high
+console.log("anil")
+
+let name= "anil"
+let age= 19
+let isLoggedIn= false
+
+// number => 2 to power 53
+// big int => used for big numbers,such as stock market,trading,reddit
+// String =>"Mullangi Tejasree AnilKumar"
+//boolean => true/false
+//null => standalone value(representation of empty value)
+//undefined=which value is not defined
+//symbol => to find uniqueness
+
+//object =>
+
+console.log(typeof undefined) //undefined, a type of js
+console.log(typeof null) //object
