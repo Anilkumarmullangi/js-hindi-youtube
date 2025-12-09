f62807206e7a0e089f197a59271f4ec541fb81f9
Anil Kumar Mullangi <160340131+Anilkumarmullangi@users.noreply.github.com>
2025-12-09 06:12:11 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,19 @@
+// console.log( 2 > 1)
+// console.log( 2 >= 1)
+// console.log( 2 == 1)
+// console.log( 2 < 1)
+// console.log( 2 != 1)
+
+/* puts under confusion 
+// console.log( "2" > 1)
+// console.log( null > 0)
+// console.log( null == 0)
+// console.log( null >= 0)
+
+// console.log( undefined == 0 )
+// console.log( undefined < 0 )
+// console.log( undefined >= 0 )
+
+*/
+// === triple check || strict checks => checks the datatypes and equal string 
+console.log(" 2 " === " 2 ")
