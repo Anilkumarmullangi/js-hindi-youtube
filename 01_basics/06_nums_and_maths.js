3770554b7a0e4071e12e66fde4a111df0357ae34
Anil Kumar Mullangi <160340131+Anilkumarmullangi@users.noreply.github.com>
2025-12-25 18:27:48 +0000

    num and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score =700
+// console.log(score);
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const anotherNumber = 128.8966
+
+// console.log(anotherNumber.toPrecision(3));
+const curr= 10000000
+// console.log(curr.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-9))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+// console.log(Math.min(4,2,5,6,4));
+console.log((Math.random()*10)+1);//to remove that 0.0.. coming case
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
